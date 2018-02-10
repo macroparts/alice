8c392d171309653397a2b0edb4bafc2de367583a
Florian Holzmann <florian.holzmann@innosabi.com>
2018-02-10 19:50:37 +0100

    First Template for Wall

--- app/resources/views/app.blade.php
+++ app/resources/views/app.blade.php
@@ -6,6 +6,10 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
 </head>
 <body>
 
